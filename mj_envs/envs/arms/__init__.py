4a47992120c5f4402af55c4f6d58de4a5dc0be51
Vikash Kumar <vikashplus@gmail.com>
2020-11-02 08:10:35 -0800

    FEATURE: Fetch and Franka sims and envs

---
+++ mj_envs/envs/arms/__init__.py
@@ -0,0 +1,39 @@
+from gym.envs.registration import register
+
+
+# FRANKA =======================================================================
+
+# Reach to fixed target
+register(
+    id='FrankaReachFixed-v0',
+    entry_point='mj_envs.envs.arms.franka.reach_v0:FrankaReachFixed',
+    max_episode_steps=50, #50steps*40Skip*2ms = 4s
+)
+from mj_envs.envs.arms.franka.reach_v0 import FrankaReachFixed
+
+# Reach to random target
+register(
+    id='FrankaReachRandom-v0',
+    entry_point='mj_envs.envs.arms.franka.reach_v0:FrankaReachRandom',
+    max_episode_steps=50, #50steps*40Skip*2ms = 4s
+)
+from mj_envs.envs.arms.franka.reach_v0 import FrankaReachRandom
+
+
+# FETCH =======================================================================
+
+# Reach to fixed target
+register(
+    id='FetchReachFixed-v0',
+    entry_point='mj_envs.envs.arms.fetch.reach_v0:FetchReachFixed',
+    max_episode_steps=50, #50steps*40Skip*2ms = 4s
+)
+from mj_envs.envs.arms.fetch.reach_v0 import FetchReachFixed
+
+# Reach to random target
+register(
+    id='FetchReachRandom-v0',
+    entry_point='mj_envs.envs.arms.fetch.reach_v0:FetchReachRandom',
+    max_episode_steps=50, #50steps*40Skip*2ms = 4s
+)
+from mj_envs.envs.arms.fetch.reach_v0 import FetchReachRandom
