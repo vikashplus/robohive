d11c13a868232fa032aa7416dd487e5801308bab
Vikash Kumar <vikashplus@gmail.com>
2021-03-09 18:17:46 -0800

    MINOR: Typo

--- mj_envs/envs/arms/franka/reach_v0.py
+++ mj_envs/envs/arms/franka/reach_v0.py
@@ -5,7 +5,7 @@ class FrankaReachFixed(ReachEnvFixed):
 
     def __init__(self):
         curr_dir = os.path.dirname(os.path.abspath(__file__))+'/'
-        ReachEnvRandom.__init__(self,
+        ReachEnvFixed.__init__(self,
             model_path = '/franka/assets/franka_reach_v0.xml',
             config_path = curr_dir + '/assets/franka_reach_v0.config',
             robot_site_name="end_effector",
