63bdb135d1877991cba39cdac4af10f60fb0f019
Aravind Rajeswaran <aravraj@fb.com>
2021-06-17 16:09:40 -0700

    Adding v3 version of environments with only robot, object, and goal observations

--- mj_envs/envs/relay_kitchen/__init__.py
+++ mj_envs/envs/relay_kitchen/__init__.py
@@ -1,3 +1,5 @@
+from mj_envs.envs.relay_kitchen.kitchen_multitask_v1 import KitchenTasksV0
+from mj_envs.envs.relay_kitchen.kitchen_multitask_v2 import KitchenFetchFixed
 from gym.envs.registration import register
 import numpy as np
 
@@ -7,7 +9,6 @@ register(
     entry_point='mj_envs.envs.relay_kitchen:KitchenTasksV0',
     max_episode_steps=280,
 )
-from mj_envs.envs.relay_kitchen.kitchen_multitask_v1 import KitchenTasksV0
 
 # Kitchen
 register(
@@ -15,7 +16,6 @@ register(
     entry_point='mj_envs.envs.relay_kitchen:KitchenFetchFixed',
     max_episode_steps=280,
 )
-from mj_envs.envs.relay_kitchen.kitchen_multitask_v2 import KitchenFetchFixed
 
 # Open Microwave door
 register(
@@ -27,7 +27,6 @@ register(
                 'interact_site': "microhandle_site"
             }
 )
-from mj_envs.envs.relay_kitchen.kitchen_multitask_v2 import KitchenFetchFixed
 
 # Open right hinge cabinet
 register(
@@ -39,7 +38,6 @@ register(
                 'interact_site': "hinge_site2"
             }
 )
-from mj_envs.envs.relay_kitchen.kitchen_multitask_v2 import KitchenFetchFixed
 
 # Open left hinge cabinet
 register(
@@ -51,7 +49,6 @@ register(
                 'interact_site': "hinge_site1"
             }
 )
-from mj_envs.envs.relay_kitchen.kitchen_multitask_v2 import KitchenFetchFixed
 
 # Open slide cabinet
 register(
@@ -63,7 +60,6 @@ register(
                 'interact_site': "slide_site"
             }
 )
-from mj_envs.envs.relay_kitchen.kitchen_multitask_v2 import KitchenFetchFixed
 
 # Lights on (left)
 register(
@@ -75,7 +71,6 @@ register(
                 'interact_site': "light_site"
             }
 )
-from mj_envs.envs.relay_kitchen.kitchen_multitask_v2 import KitchenFetchFixed
 
 # Knob4 on
 register(
@@ -87,7 +82,6 @@ register(
                 'interact_site': "knob4_site"
             }
 )
-from mj_envs.envs.relay_kitchen.kitchen_multitask_v2 import KitchenFetchFixed
 
 # Knob3 on
 register(
@@ -99,7 +93,6 @@ register(
                 'interact_site': "knob3_site"
             }
 )
-from mj_envs.envs.relay_kitchen.kitchen_multitask_v2 import KitchenFetchFixed
 
 # Knob2 on
 register(
@@ -111,7 +104,6 @@ register(
                 'interact_site': "knob2_site"
             }
 )
-from mj_envs.envs.relay_kitchen.kitchen_multitask_v2 import KitchenFetchFixed
 
 # Knob1 on
 register(
@@ -123,4 +115,128 @@ register(
                 'interact_site': "knob1_site"
             }
 )
-from mj_envs.envs.relay_kitchen.kitchen_multitask_v2 import KitchenFetchFixed
+
+
+# ========================================================
+
+# V3 environments
+# This version contains fewer observation keys
+# In particular, observations only consist of robot pose, object pose, and goals
+
+# Kitchen
+register(
+    id='kitchen-v3',
+    entry_point='mj_envs.envs.relay_kitchen:KitchenFetchFixed',
+    max_episode_steps=280,
+    kwargs={
+                'obs_keys_wt': {"hand_jnt": 1.0, "objs_jnt": 1.0, "goal": 1.0},
+           }
+)
+
+# Open Microwave door
+register(
+    id='kitchen_micro_open-v3',
+    entry_point='mj_envs.envs.relay_kitchen:KitchenFetchFixed',
+    max_episode_steps=50,
+    kwargs={
+                'goal': np.array([0, 0, 0, 0, 0, 0, 0, 0, -2.19]),
+                'obs_keys_wt': {"hand_jnt": 1.0, "objs_jnt": 1.0, "goal": 1.0},
+                'interact_site': "microhandle_site"
+            }
+)
+
+# Open right hinge cabinet
+register(
+    id='kitchen_rdoor_open-v3',
+    entry_point='mj_envs.envs.relay_kitchen:KitchenFetchFixed',
+    max_episode_steps=50,
+    kwargs={
+                'goal': np.array([0, 0, 0, 0, 0, 0, 0, 1.57, 0]),
+                'obs_keys_wt': {"hand_jnt": 1.0, "objs_jnt": 1.0, "goal": 1.0},
+                'interact_site': "hinge_site2"
+            }
+)
+
+# Open left hinge cabinet
+register(
+    id='kitchen_ldoor_open-v3',
+    entry_point='mj_envs.envs.relay_kitchen:KitchenFetchFixed',
+    max_episode_steps=50,
+    kwargs={
+                'goal': np.array([0, 0, 0, 0, 0, 0, -1.57, 0, 0]),
+                'obs_keys_wt': {"hand_jnt": 1.0, "objs_jnt": 1.0, "goal": 1.0},
+                'interact_site': "hinge_site1"
+            }
+)
+
+# Open slide cabinet
+register(
+    id='kitchen_sdoor_open-v3',
+    entry_point='mj_envs.envs.relay_kitchen:KitchenFetchFixed',
+    max_episode_steps=50,
+    kwargs={
+                'goal': np.array([0, 0, 0, 0, 0, 0.44, 0, 0, 0]),
+                'obs_keys_wt': {"hand_jnt": 1.0, "objs_jnt": 1.0, "goal": 1.0},
+                'interact_site': "slide_site"
+            }
+)
+
+# Lights on (left)
+register(
+    id='kitchen_light_on-v3',
+    entry_point='mj_envs.envs.relay_kitchen:KitchenFetchFixed',
+    max_episode_steps=50,
+    kwargs={
+                'goal': np.array([0, 0, 0, 0, -.7, 0, 0, 0, 0]),
+                'obs_keys_wt': {"hand_jnt": 1.0, "objs_jnt": 1.0, "goal": 1.0},
+                'interact_site': "light_site"
+            }
+)
+
+# Knob4 on
+register(
+    id='kitchen_knob4_on-v3',
+    entry_point='mj_envs.envs.relay_kitchen:KitchenFetchFixed',
+    max_episode_steps=50,
+    kwargs={
+                'goal': np.array([0, 0, 0, -1.57, 0, 0, 0, 0, 0]),
+                'obs_keys_wt': {"hand_jnt": 1.0, "objs_jnt": 1.0, "goal": 1.0},
+                'interact_site': "knob4_site"
+            }
+)
+
+# Knob3 on
+register(
+    id='kitchen_knob3_on-v3',
+    entry_point='mj_envs.envs.relay_kitchen:KitchenFetchFixed',
+    max_episode_steps=50,
+    kwargs={
+                'goal': np.array([0, 0, -1.57, 0, 0, 0, 0, 0, 0]),
+                'obs_keys_wt': {"hand_jnt": 1.0, "objs_jnt": 1.0, "goal": 1.0},
+                'interact_site': "knob3_site"
+            }
+)
+
+# Knob2 on
+register(
+    id='kitchen_knob2_on-v3',
+    entry_point='mj_envs.envs.relay_kitchen:KitchenFetchFixed',
+    max_episode_steps=50,
+    kwargs={
+                'goal': np.array([0, -1.57, 0, 0, 0, 0, 0, 0, 0]),
+                'obs_keys_wt': {"hand_jnt": 1.0, "objs_jnt": 1.0, "goal": 1.0},
+                'interact_site': "knob2_site"
+            }
+)
+
+# Knob1 on
+register(
+    id='kitchen_knob1_on-v3',
+    entry_point='mj_envs.envs.relay_kitchen:KitchenFetchFixed',
+    max_episode_steps=50,
+    kwargs={
+                'goal': np.array([-1.57, 0, 0, 0, 0, 0, 0, 0, 0]),
+                'obs_keys_wt': {"hand_jnt": 1.0, "objs_jnt": 1.0, "goal": 1.0},
+                'interact_site': "knob1_site"
+            }
+)
