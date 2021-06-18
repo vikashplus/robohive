Bug fixes to success evaluation - Success computation may be incorrect when adding bool values, converted them to float - Relaxed success criteria for microwave and left door, since these are outside the reach of the robot - Relaxed goal threshold criteria from 0.1*range to 0.15*range

mj_envs/envs/env_base.py
@@ -214,7 +214,8 @@ class MujocoEnv(gym.Env, utils.EzPickle, ObsVecDict):
 
         # Record success if solved for provided successful_steps
         for path in paths:
-            if np.sum(path['env_infos']['solved']) > successful_steps:
+            if np.sum(path['env_infos']['solved'] * 1.0) > successful_steps:
+                # sum of truth values may not work correctly if dtype=object, need to * 1.0
                 num_success += 1
         success_percentage = num_success*100.0/num_paths
 