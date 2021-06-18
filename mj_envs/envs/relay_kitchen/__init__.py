Bug fixes to success evaluation - Success computation may be incorrect when adding bool values, converted them to float - Relaxed success criteria for microwave and left door, since these are outside the reach of the robot - Relaxed goal threshold criteria from 0.1*range to 0.15*range

mj_envs/envs/relay_kitchen/__init__.py
@@ -23,7 +23,7 @@ register(
     entry_point='mj_envs.envs.relay_kitchen:KitchenFetchFixed',
     max_episode_steps=50,
     kwargs={
-                'goal': np.array([0, 0, 0, 0, 0, 0, 0, 0, -2.19]),
+                'goal': np.array([0, 0, 0, 0, 0, 0, 0, 0, -1.25]),
                 'interact_site': "microhandle_site"
             }
 )
@@ -45,7 +45,7 @@ register(
     entry_point='mj_envs.envs.relay_kitchen:KitchenFetchFixed',
     max_episode_steps=50,
     kwargs={
-                'goal': np.array([0, 0, 0, 0, 0, 0, -1.57, 0, 0]),
+                'goal': np.array([0, 0, 0, 0, 0, 0, -1.25, 0, 0]),
                 'interact_site': "hinge_site1"
             }
 )
@@ -139,7 +139,7 @@ register(
     entry_point='mj_envs.envs.relay_kitchen:KitchenFetchFixed',
     max_episode_steps=50,
     kwargs={
-                'goal': np.array([0, 0, 0, 0, 0, 0, 0, 0, -2.19]),
+                'goal': np.array([0, 0, 0, 0, 0, 0, 0, 0, -1.25]),
                 'obs_keys_wt': {"hand_jnt": 1.0, "objs_jnt": 1.0, "goal": 1.0},
                 'interact_site': "microhandle_site"
             }
@@ -163,7 +163,7 @@ register(
     entry_point='mj_envs.envs.relay_kitchen:KitchenFetchFixed',
     max_episode_steps=50,
     kwargs={
-                'goal': np.array([0, 0, 0, 0, 0, 0, -1.57, 0, 0]),
+                'goal': np.array([0, 0, 0, 0, 0, 0, -1.25, 0, 0]),
                 'obs_keys_wt': {"hand_jnt": 1.0, "objs_jnt": 1.0, "goal": 1.0},
                 'interact_site': "hinge_site1"
             }

mj_envs/envs/relay_kitchen/kitchen_multitask_v2.py
@@ -107,7 +107,7 @@ class KitchenBase(env_base.MujocoEnv):
             ('approach',-np.linalg.norm(obs_dict['approach_err'], axis=-1)),
             # Must keys
             ('sparse',  -np.sum(goal_dist, axis=-1)),
-            ('solved',  np.all(goal_dist < 0.1*self.obj_ranges)),
+            ('solved',  np.all(goal_dist < 0.15*self.obj_ranges)),
             ('done',    False),
         ))
         rwd_dict['dense'] = np.sum([wt*rwd_dict[key] for key, wt in self.rwd_keys_wt.items()], axis=0)