470684a1aa06c592b10381a5013318775353bc91
Vikash Kumar <vikashplus@gmail.com>
2021-06-26 10:15:48 -0700

    FEATURE: RoboPenV0 env. - healthy env - D'Manus not mounted on the arm yet

--- mj_envs/envs/fm/__init__.py
+++ mj_envs/envs/fm/__init__.py
@@ -5,13 +5,24 @@ curr_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Reach to fixed target
 register(
-    id='FMReachFixed-v0',
+    id='DManusReachFixed-v0',
     entry_point='mj_envs.envs.fm.base_v0:FMReachEnvFixed',
-    max_episode_steps=5000, #50steps*40Skip*2ms = 4s
+    max_episode_steps=50, #50steps*40Skip*2ms = 4s
     kwargs={
-            'model_path': '/assets/dmanus.mjb',
+            'model_path': '/assets/dmanus.xml',
             'config_path': curr_dir+'/assets/dmanus.config',
             'target_pose': np.array([0, 1, 1, 0, 1, 1, 0, 1, 1]),
         }
 )
-from mj_envs.envs.arms.franka.reach_v0 import FrankaReachFixed
+
+# Reach to fixed target
+register(
+    id='FMReachFixed-v0',
+    entry_point='mj_envs.envs.fm.base_v0:FMReachEnvFixed',
+    max_episode_steps=50, #50steps*40Skip*2ms = 4s
+    kwargs={
+            'model_path': '/assets/robopen.xml',
+            'config_path': curr_dir+'/assets/robopen.config',
+            'target_pose': np.array([0, 0, 0, -1.57, 0,0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1]),
+        }
+)
