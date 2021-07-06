87a077d8f6e4d83b6a411751da1baedb7b0de3ec
Vikash Kumar <vikashplus@gmail.com>
2021-07-05 21:25:41 -0700

    FEATURE: robopen-v0.10 update - Franka arms and grippers separated - updating all envs to reflect changes - Programatically editing the XMLs to mount dmanus on frannka arm - Push with all SF updates

--- mj_envs/envs/fm/__init__.py
+++ mj_envs/envs/fm/__init__.py
@@ -4,16 +4,16 @@ import os
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Reach to fixed target
-register(
-    id='DManusReachFixed-v0',
-    entry_point='mj_envs.envs.fm.base_v0:FMReachEnvFixed',
-    max_episode_steps=50, #50steps*40Skip*2ms = 4s
-    kwargs={
-            'model_path': '/assets/dmanus.xml',
-            'config_path': curr_dir+'/assets/dmanus.config',
-            'target_pose': np.array([0, 1, 1, 0, 1, 1, 0, 1, 1]),
-        }
-)
+# register(
+#     id='DManusReachFixed-v0',
+#     entry_point='mj_envs.envs.fm.base_v0:FMReachEnvFixed',
+#     max_episode_steps=50, #50steps*40Skip*2ms = 4s
+#     kwargs={
+#             'model_path': '/assets/dmanus.xml',
+#             'config_path': curr_dir+'/assets/dmanus.config',
+#             'target_pose': np.array([0, 1, 1, 0, 1, 1, 0, 1, 1]),
+#         }
+# )
 
 # Reach to fixed target
 register(
@@ -23,6 +23,6 @@ register(
     kwargs={
             'model_path': '/assets/robopen.xml',
             'config_path': curr_dir+'/assets/robopen.config',
-            'target_pose': np.array([0, 0, 0, -1.57, 0,0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1]),
+            'target_pose': np.array([0, 0, 0, -1.57, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1]),
         }
 )
