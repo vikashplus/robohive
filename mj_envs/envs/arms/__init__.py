bdf5190c76586ef48ac8f79f9eb93619f3c5e583
Vikash Kumar <vikashplus@gmail.com>
2021-07-15 23:10:50 -0400

    FEATURE: Adding Push Env + Cleaning Reach Envs - Clearning up inheritance chain for reach envs - Passing randomization range as arguments, removing the need for explict fixed/ random definitions - Adding Franka Push envs - Using TxyzRxyz instead of freejoint in push envs - Add envs passing test

--- mj_envs/envs/arms/__init__.py
+++ mj_envs/envs/arms/__init__.py
@@ -1,39 +1,100 @@
 from gym.envs.registration import register
+import os
+curr_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-# FRANKA =======================================================================
+# FRANKA REACH =======================================================================
+from mj_envs.envs.arms.reach_base_v0 import ReachBaseV0
 
 # Reach to fixed target
 register(
     id='FrankaReachFixed-v0',
-    entry_point='mj_envs.envs.arms.franka.reach_v0:FrankaReachFixed',
+    entry_point='mj_envs.envs.arms.reach_base_v0:ReachBaseV0',
     max_episode_steps=50, #50steps*40Skip*2ms = 4s
+    kwargs={
+        'model_path': curr_dir+'/franka/assets/franka_reach_v0.xml',
+        'config_path': curr_dir+'/franka/assets/franka_reach_v0.config',
+        'robot_site_name': "end_effector",
+        'target_site_name': "target",
+        'target_xyz_range': {'high':[0.2, 0.3, 1.2], 'low':[0.2, 0.3, 1.2]}
+    }
 )
-from mj_envs.envs.arms.franka.reach_v0 import FrankaReachFixed
 
 # Reach to random target
 register(
     id='FrankaReachRandom-v0',
-    entry_point='mj_envs.envs.arms.franka.reach_v0:FrankaReachRandom',
+    entry_point='mj_envs.envs.arms.reach_base_v0:ReachBaseV0',
     max_episode_steps=50, #50steps*40Skip*2ms = 4s
+    kwargs={
+        'model_path': curr_dir+'/franka/assets/franka_reach_v0.xml',
+        'config_path': curr_dir+'/franka/assets/franka_reach_v0.config',
+        'robot_site_name': "end_effector",
+        'target_site_name': "target",
+        'target_xyz_range': {'high':[0.3, .5, 1.2], 'low':[-.3, .1, .8]}
+    }
 )
-from mj_envs.envs.arms.franka.reach_v0 import FrankaReachRandom
+
+# FRANKA PUSH =======================================================================
+from mj_envs.envs.arms.push_base_v0 import PushBaseV0
+
+# Push object to target
+register(
+    id='FrankaPushFixed-v0',
+    entry_point='mj_envs.envs.arms.push_base_v0:PushBaseV0',
+    max_episode_steps=50, #50steps*40Skip*2ms = 4s
+    kwargs={
+        'model_path': curr_dir+'/franka/assets/franka_ycb_v0.xml',
+        'config_path': curr_dir+'/franka/assets/franka_ycb_v0.config',
+        'robot_site_name': "end_effector",
+        'object_site_name': "sugarbox",
+        'target_site_name': "target",
+        'target_xyz_range': {'high':[-.4, 0.5, 0.78], 'low':[-.4, 0.5, 0.78]}
+    }
+)
+
+# Push object to target
+register(
+    id='FrankaPushRandom-v0',
+    entry_point='mj_envs.envs.arms.push_base_v0:PushBaseV0',
+    max_episode_steps=50, #50steps*40Skip*2ms = 4s
+    kwargs={
+        'model_path': curr_dir+'/franka/assets/franka_ycb_v0.xml',
+        'config_path': curr_dir+'/franka/assets/franka_ycb_v0.config',
+        'robot_site_name': "end_effector",
+        'object_site_name': "sugarbox",
+        'target_site_name': "target",
+        'target_xyz_range': {'high':[0.4, 0.5, 0.78], 'low':[-.4, .4, 0.78]}
+    }
+)
 
 
 # FETCH =======================================================================
+from mj_envs.envs.arms.reach_base_v0 import ReachBaseV0
 
 # Reach to fixed target
 register(
     id='FetchReachFixed-v0',
-    entry_point='mj_envs.envs.arms.fetch.reach_v0:FetchReachFixed',
+    entry_point='mj_envs.envs.arms.reach_base_v0:ReachBaseV0',
     max_episode_steps=50, #50steps*40Skip*2ms = 4s
+    kwargs={
+        'model_path': curr_dir+'/fetch/assets/fetch_reach_v0.xml',
+        'config_path': curr_dir+'/fetch/assets/fetch_reach_v0.config',
+        'robot_site_name': "grip",
+        'target_site_name': "target",
+        'target_xyz_range': {'high':[0.2, 0.3, 1.2], 'low':[0.2, 0.3, 1.2]}
+    }
 )
-from mj_envs.envs.arms.fetch.reach_v0 import FetchReachFixed
 
 # Reach to random target
 register(
     id='FetchReachRandom-v0',
-    entry_point='mj_envs.envs.arms.fetch.reach_v0:FetchReachRandom',
+    entry_point='mj_envs.envs.arms.reach_base_v0:ReachBaseV0',
     max_episode_steps=50, #50steps*40Skip*2ms = 4s
+    kwargs={
+        'model_path': curr_dir+'/fetch/assets/fetch_reach_v0.xml',
+        'config_path': curr_dir+'/fetch/assets/fetch_reach_v0.config',
+        'robot_site_name': "grip",
+        'target_site_name': "target",
+        'target_xyz_range': {'high':[0.3, .5, 1.2], 'low':[-.3, .1, .8]}
+    }
 )
-from mj_envs.envs.arms.fetch.reach_v0 import FetchReachRandom
