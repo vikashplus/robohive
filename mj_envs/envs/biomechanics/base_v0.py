719db5f7bab47526b4f1c3019c421c6325c03b91
Giri Anantharaman <giriman@devfair0439.h2.fair>
2021-07-25 15:04:54 -0700

    First pass at pickle fixes

--- mj_envs/envs/biomechanics/base_v0.py
+++ mj_envs/envs/biomechanics/base_v0.py
@@ -1,21 +1,26 @@
 from mj_envs.envs import env_base
 from mujoco_py import MjViewer
 import numpy as np
+import gym
 
 class BaseV0(env_base.MujocoEnv):
 
-    def __init__(self, sim=None, sim_obsd=None, model_path=None, # model details
-                    sites:tuple = None, # fingertip sites of interests
-                    obs_keys:list=None,
-                    frame_skip = 10,
-                    **kwargs):
+    def __init__(self, model_path):
+        super().__init__(model_path)
 
-        # get sim
-        if sim is None:
-            sim = env_base.get_sim(model_path=model_path)
-            sim_obsd = env_base.get_sim(model_path=model_path)
+    def _setup(self,
+            obs_keys:list,
+            weighted_reward_keys:dict,
+            sites:list = None,
+            frame_skip = 10,
+            seed = None,
+            is_hardware = False,
+            config_path = None,
+            rwd_viz = False,
+            normalize_act = True,
+        ):
 
-        if sim.model.na>0 and 'act' not in obs_keys:
+        if self.sim.model.na>0 and 'act' not in obs_keys:
             obs_keys.append('act')
 
         # ids
@@ -23,27 +28,22 @@ class BaseV0(env_base.MujocoEnv):
         self.target_sids = []
         if sites:
             for site in sites:
-                self.tip_sids.append(sim.model.site_name2id(site))
-                self.target_sids.append(sim.model.site_name2id(site+'_target'))
-
-        # get env
-        env_base.MujocoEnv.__init__(self,
-                                sim = sim,
-                                sim_obsd = sim_obsd,
-                                model_path = model_path,
-                                frame_skip = frame_skip,
-                                config_path = None,
-                                obs_keys = obs_keys,
-                                rwd_mode = "dense",
-                                act_mode = "pos",
-                                act_normalized = True,
-                                is_hardware = False,
-                                **kwargs)
+                self.tip_sids.append(self.sim.model.site_name2id(site))
+                self.target_sids.append(self.sim.model.site_name2id(site+'_target'))
+        
+        super()._setup(obs_keys=obs_keys, 
+                    weighted_reward_keys=weighted_reward_keys, 
+                    frame_skip=frame_skip, 
+                    seed=seed, 
+                    is_hardware=is_hardware,
+                    config_path=config_path,
+                    rwd_viz=rwd_viz,
+                    normalize_act=normalize_act)
 
 
     # step the simulation forward
     def step(self, a):
-        if self.act_normalized:
+        if self.normalize_act:
             a = 1.0/(1.0+np.exp(-5.0*(a-0.5)))
         return super().step(a=a)
 
