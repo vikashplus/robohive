Major: qpos_init, qvel*dt, logocal/numerical bugfix + env adjust - overwritting init_qpos to open hand for some tasks - qvel is scaled with dt now - BUGFIX: addition-operation in rwd needed to be numerical not logical - Updates to viewer setup - Keyturn: success relaxed from 2pi to pi + sparse rwd moved to qpos - Baoding: Bugfix in object fall detection

mj_envs/envs/biomechanics/baoding_v1.py
@@ -25,11 +25,11 @@ class BaodingFixedEnvV1(BaseV0):
 
     DEFAULT_OBS_KEYS = ['hand_pos', 'object1_pos', 'object1_velp', 'object2_pos', 'object2_velp', 'target1_pos', 'target2_pos']
     DEFAULT_RWD_KEYS_AND_WEIGHTS = {
-       'pos_dist_1':-5.0,
-       'pos_dist_2':-5.0,
-       'drop_penalty':-500.0,
-       'wrist_angle':-0.5,
-       'act_reg':-.1,
+       'pos_dist_1':5.0,
+       'pos_dist_2':5.0,
+       'drop_penalty':500.0,
+       'wrist_angle':0.5,
+       'act_reg':1.0,
        'bonus':1.0
     }
 
@@ -49,17 +49,17 @@ class BaodingFixedEnvV1(BaseV0):
         # Also see: https://github.com/openai/gym/pull/1497
         gym.utils.EzPickle.__init__(**locals())
 
-        # This two step construction is required for pickling to work correctly. All arguments to all __init__ 
-        # calls must be pickle friendly. Things like sim / sim_obsd are NOT pickle friendly. Therefore we 
+        # This two step construction is required for pickling to work correctly. All arguments to all __init__
+        # calls must be pickle friendly. Things like sim / sim_obsd are NOT pickle friendly. Therefore we
         # first construct the inheritance chain, which is just __init__ calls all the way down, with env_base
         # creating the sim / sim_obsd instances. Next we run through "setup"  which relies on sim / sim_obsd
         # created in __init__ to complete the setup.
         super().__init__(model_path=model_path)
 
-        self._setup(obs_keys=obs_keys, 
-                    weighted_reward_keys=weighted_reward_keys, 
-                    normalize_act=normalize_act, 
-                    reward_option=reward_option, 
+        self._setup(obs_keys=obs_keys,
+                    weighted_reward_keys=weighted_reward_keys,
+                    normalize_act=normalize_act,
+                    reward_option=reward_option,
                     rwd_viz=False,
                     seed=seed)
 
@@ -83,7 +83,7 @@ class BaodingFixedEnvV1(BaseV0):
         #   1= yellow = top right
         #   2= pink = bottom left
         self.ball_1_starting_angle = 3.*np.pi/4.0
-        self.ball_2_starting_angle = -1*np.pi/4.0
+        self.ball_2_starting_angle = -1.*np.pi/4.0
         # init desired trajectory, for baoding
         self.x_radius = 0.025 #0.03
         self.y_radius = 0.028 #0.02 * 1.5 * 1.2
@@ -98,11 +98,13 @@ class BaodingFixedEnvV1(BaseV0):
         self.target1_sid = self.sim.model.site_name2id('target1_site')
         self.target2_sid = self.sim.model.site_name2id('target2_site')
 
-        super()._setup(obs_keys=obs_keys, 
-                    weighted_reward_keys=weighted_reward_keys, 
-                    normalize_act=normalize_act, 
+        super()._setup(obs_keys=obs_keys,
+                    weighted_reward_keys=weighted_reward_keys,
+                    normalize_act=normalize_act,
                     rwd_viz=rwd_viz,
                     seed=seed)
+
+        self.init_qpos[:-14] *= 0 # Use fully open as init pos
 
 
     def step(self, a):
@@ -176,8 +178,8 @@ class BaodingFixedEnvV1(BaseV0):
         obs_dict['object2_pos'] = sim.data.site_xpos[self.object2_sid].copy()
 
         # object translational velocities
-        obs_dict['object1_velp']   = sim.data.qvel[-12:-9].copy()
-        obs_dict['object2_velp']   = sim.data.qvel[-6:-3].copy()
+        obs_dict['object1_velp']   = sim.data.qvel[-12:-9].copy()*self.dt
+        obs_dict['object2_velp']   = sim.data.qvel[-6:-3].copy()*self.dt
 
         # site locations in world frame, populated after the step/forward call
         # obs_dict['target1_pos'] = np.array([sim.data.site_xpos[self.target1_sid][0], sim.data.site_xpos[self.target1_sid][1]])
@@ -201,6 +203,7 @@ class BaodingFixedEnvV1(BaseV0):
         # tracking error
         target1_dist = np.linalg.norm(obs_dict['target1_err'], axis=-1)
         target2_dist = np.linalg.norm(obs_dict['target2_err'], axis=-1)
+        act_mag = np.linalg.norm(self.obs_dict['act'], axis=-1)/self.sim.model.na if self.sim.model.na !=0 else 0
 
         # wrist pose err
         hand_pos = obs_dict['hand_pos'][:,:,:3] if obs_dict['hand_pos'].ndim==3 else obs_dict['hand_pos'][:3]
@@ -208,19 +211,19 @@ class BaodingFixedEnvV1(BaseV0):
 
         # detect fall
         object1_pos = obs_dict['object1_pos'][:,:,2] if obs_dict['object1_pos'].ndim==3 else obs_dict['object1_pos'][2]
-        object1_pos = obs_dict['object2_pos'][:,:,2] if obs_dict['object2_pos'].ndim==3 else obs_dict['object2_pos'][2]
+        object2_pos = obs_dict['object2_pos'][:,:,2] if obs_dict['object2_pos'].ndim==3 else obs_dict['object2_pos'][2]
         is_fall_1 = object1_pos < self.drop_height_threshold
-        is_fall_2 = object1_pos < self.drop_height_threshold
+        is_fall_2 = object2_pos < self.drop_height_threshold
         is_fall = np.logical_or(is_fall_1, is_fall_2) #keep both balls up
 
         rwd_dict = collections.OrderedDict((
             # Optional Keys
-            ('pos_dist_1',      target1_dist),
-            ('pos_dist_2',      target2_dist),
-            ('drop_penalty',    is_fall),
-            ('wrist_angle',     wrist_pose_err),
-            ('act_reg',         np.linalg.norm(self.obs_dict['act'], axis=-1)),
-            ('bonus',           (target1_dist < self.proximity_threshold)+(target2_dist < self.proximity_threshold)+4*(target1_dist < self.proximity_threshold)*(target2_dist < self.proximity_threshold)),
+            ('pos_dist_1',      -1.*target1_dist),
+            ('pos_dist_2',      -1.*target2_dist),
+            ('drop_penalty',    -1.*is_fall),
+            ('wrist_angle',     -1.*wrist_pose_err),
+            ('act_reg',         -1.*act_mag),
+            ('bonus',           1.*(target1_dist < self.proximity_threshold)+1.*(target2_dist < self.proximity_threshold)+4.*(target1_dist < self.proximity_threshold)*(target2_dist < self.proximity_threshold)),
             # Must keys
             ('sparse',          -target1_dist-target2_dist),
             ('solved',          (target1_dist < self.proximity_threshold)*(target2_dist < self.proximity_threshold)*(~is_fall)),
@@ -349,7 +352,7 @@ class BaodingFixedEnvV1(BaseV0):
             rwd_dense = np.mean([np.sum(p['env_infos']['rwd_dense'])/horizon for p in paths]) # return rwd/step
             logger.log_kv('rwd_sparse', rwd_sparse)
             logger.log_kv('rwd_dense', rwd_dense)
-            logger.log_kv('success_rate', success_percentage)
+            logger.log_kv('success_percentage', success_percentage)
 
         return success_percentage
 

mj_envs/envs/biomechanics/key_turn_v0.py
@@ -10,10 +10,10 @@ class KeyTurnFixedEnvV0(BaseV0):
     DEFAULT_OBS_KEYS = ['hand_qpos', 'hand_qvel', 'key_qpos', 'key_qvel', 'IFtip_approach', 'THtip_approach']
     DEFAULT_RWD_KEYS_AND_WEIGHTS= {
         'key_turn':1.0,
-        'IFtip_approach':-100.0,
-        'THtip_approach':-100.0,
+        'IFtip_approach':100.0,
+        'THtip_approach':100.0,
         'bonus':4.0,
-        'penalty':-25.0
+        'penalty':25.0
     }
 
     def __init__(self,
@@ -31,17 +31,17 @@ class KeyTurnFixedEnvV0(BaseV0):
         # Also see: https://github.com/openai/gym/pull/1497
         gym.utils.EzPickle.__init__(**locals())
 
-        # This two step construction is required for pickling to work correctly. All arguments to all __init__ 
-        # calls must be pickle friendly. Things like sim / sim_obsd are NOT pickle friendly. Therefore we 
+        # This two step construction is required for pickling to work correctly. All arguments to all __init__
+        # calls must be pickle friendly. Things like sim / sim_obsd are NOT pickle friendly. Therefore we
         # first construct the inheritance chain, which is just __init__ calls all the way down, with env_base
         # creating the sim / sim_obsd instances. Next we run through "setup"  which relies on sim / sim_obsd
         # created in __init__ to complete the setup.
         super().__init__(model_path=model_path)
 
-        self._setup(obs_keys=obs_keys, 
-                    weighted_reward_keys=weighted_reward_keys, 
-                    normalize_act=normalize_act, 
-                    rwd_viz=False, 
+        self._setup(obs_keys=obs_keys,
+                    weighted_reward_keys=weighted_reward_keys,
+                    normalize_act=normalize_act,
+                    rwd_viz=False,
                     seed=seed)
 
     def _setup(self,
@@ -53,14 +53,15 @@ class KeyTurnFixedEnvV0(BaseV0):
         ):
         self.keyhead_sid = self.sim.model.site_name2id("keyhead")
         self.IF_sid = self.sim.model.site_name2id("IFtip")
-        self.TH_sid = self.sim.model.site_name2id("THtip")   
-        # self.init_qpos = self.sim.model.key_qpos[0]    
+        self.TH_sid = self.sim.model.site_name2id("THtip")
+        # self.init_qpos = self.sim.model.key_qpos[0]
 
-        super()._setup(obs_keys=obs_keys, 
-                    weighted_reward_keys=weighted_reward_keys, 
-                    normalize_act=normalize_act, 
+        super()._setup(obs_keys=obs_keys,
+                    weighted_reward_keys=weighted_reward_keys,
+                    normalize_act=normalize_act,
                     rwd_viz=rwd_viz,
                     seed=seed)
+        self.init_qpos[:-1] *= 0 # Use fully open as init pos
 
     def get_obs_vec(self):
         self.obs_dict['t'] = np.array([self.sim.data.time])
@@ -81,9 +82,9 @@ class KeyTurnFixedEnvV0(BaseV0):
         obs_dict = {}
         obs_dict['t'] = np.array([sim.data.time])
         obs_dict['hand_qpos'] = sim.data.qpos[:-1].copy()
-        obs_dict['hand_qvel'] = sim.data.qvel[:-1].copy()
+        obs_dict['hand_qvel'] = sim.data.qvel[:-1].copy()*self.dt
         obs_dict['key_qpos'] = np.array([sim.data.qpos[-1]])
-        obs_dict['key_qvel'] = np.array([sim.data.qvel[-1]])
+        obs_dict['key_qvel'] = np.array([sim.data.qvel[-1]])*self.dt
         obs_dict['IFtip_approach'] = sim.data.site_xpos[self.keyhead_sid]-sim.data.site_xpos[self.IF_sid]
         obs_dict['THtip_approach'] = sim.data.site_xpos[self.keyhead_sid]-sim.data.site_xpos[self.TH_sid]
         if sim.model.na>0:
@@ -93,20 +94,21 @@ class KeyTurnFixedEnvV0(BaseV0):
     def get_reward_dict(self, obs_dict):
         IF_approach_dist = np.abs(np.linalg.norm(self.obs_dict['IFtip_approach'], axis=-1)-0.040)
         TH_approach_dist = np.abs(np.linalg.norm(self.obs_dict['THtip_approach'], axis=-1)-0.040)
-
         key_qvel = obs_dict['key_qvel'][:,:,0] if obs_dict['key_qvel'].ndim==3 else obs_dict['key_qvel'][0]
+        act_mag = np.linalg.norm(self.obs_dict['act'], axis=-1)/self.sim.model.na if self.sim.model.na !=0 else 0
         far_th = .07
 
         rwd_dict = collections.OrderedDict((
             # Optional Keys
-            ('key_turn', key_qvel),
-            ('IFtip_approach', IF_approach_dist),
-            ('THtip_approach', TH_approach_dist),
-            ('bonus', (key_qvel>np.pi*2) + (key_qvel>np.pi*3)),
-            ('penalty', (IF_approach_dist>far_th/2)+(TH_approach_dist>far_th/2) ),
+            ('key_turn', key_qvel/self.dt),
+            ('IFtip_approach', -1.*IF_approach_dist),
+            ('THtip_approach', -1.*TH_approach_dist),
+            ('act_reg', -1.*act_mag),
+            ('bonus', 1.*(key_qvel>self.dt*np.pi/2) + 1.*(key_qvel>self.dt*np.pi)),
+            ('penalty', -1.*(IF_approach_dist>far_th/2)+1.*(TH_approach_dist>far_th/2) ),
             # Must keys
-            ('sparse', key_qvel),
-            ('solved', obs_dict['key_qpos']>2*np.pi),
+            ('sparse', obs_dict['key_qpos']),
+            ('solved', obs_dict['key_qpos']>np.pi),
             ('done', (IF_approach_dist>far_th) or (TH_approach_dist>far_th)),
         ))
 

mj_envs/envs/biomechanics/obj_hold_v0.py
@@ -9,9 +9,9 @@ class ObjHoldFixedEnvV0(BaseV0):
 
     DEFAULT_OBS_KEYS = ['hand_qpos', 'hand_qvel', 'obj_pos', 'obj_err']
     DEFAULT_RWD_KEYS_AND_WEIGHTS = {
-        "goal_dist": -100.0,
+        "goal_dist": 100.0,
         "bonus": 4.0,
-        "penalty": -10,
+        "penalty": 10,
     }
 
     def __init__(self,
@@ -29,16 +29,16 @@ class ObjHoldFixedEnvV0(BaseV0):
         # Also see: https://github.com/openai/gym/pull/1497
         gym.utils.EzPickle.__init__(**locals())
 
-        # This two step construction is required for pickling to work correctly. All arguments to all __init__ 
-        # calls must be pickle friendly. Things like sim / sim_obsd are NOT pickle friendly. Therefore we 
+        # This two step construction is required for pickling to work correctly. All arguments to all __init__
+        # calls must be pickle friendly. Things like sim / sim_obsd are NOT pickle friendly. Therefore we
         # first construct the inheritance chain, which is just __init__ calls all the way down, with env_base
         # creating the sim / sim_obsd instances. Next we run through "setup"  which relies on sim / sim_obsd
         # created in __init__ to complete the setup.
         super().__init__(model_path=model_path)
 
-        self._setup(obs_keys=obs_keys, 
-                    weighted_reward_keys=weighted_reward_keys, 
-                    normalize_act=normalize_act, 
+        self._setup(obs_keys=obs_keys,
+                    weighted_reward_keys=weighted_reward_keys,
+                    normalize_act=normalize_act,
                     rwd_viz=False,
                     seed=seed)
 
@@ -53,9 +53,9 @@ class ObjHoldFixedEnvV0(BaseV0):
         self.object_sid = self.sim.model.site_name2id("object")
         self.goal_sid = self.sim.model.site_name2id("goal")
 
-        super()._setup(obs_keys=obs_keys, 
-                    weighted_reward_keys=weighted_reward_keys, 
-                    normalize_act=normalize_act, 
+        super()._setup(obs_keys=obs_keys,
+                    weighted_reward_keys=weighted_reward_keys,
+                    normalize_act=normalize_act,
                     rwd_viz=rwd_viz,
                     seed=seed)
 
@@ -63,7 +63,7 @@ class ObjHoldFixedEnvV0(BaseV0):
     def get_obs_vec(self):
         self.obs_dict['t'] = np.array([self.sim.data.time])
         self.obs_dict['hand_qpos'] = self.sim.data.qpos[:-7].copy()
-        self.obs_dict['hand_qvel'] = self.sim.data.qvel[:-6].copy()
+        self.obs_dict['hand_qvel'] = self.sim.data.qvel[:-6].copy()*self.dt
         self.obs_dict['obj_pos'] = self.sim.data.site_xpos[self.object_sid]
         self.obs_dict['obj_err'] = self.sim.data.site_xpos[self.goal_sid] - self.sim.data.site_xpos[self.object_sid]
         if self.sim.model.na>0:
@@ -76,7 +76,7 @@ class ObjHoldFixedEnvV0(BaseV0):
         obs_dict = {}
         obs_dict['t'] = np.array([sim.data.time])
         obs_dict['hand_qpos'] = sim.data.qpos[:-7].copy()
-        obs_dict['hand_qvel'] = sim.data.qvel[:-6].copy()
+        obs_dict['hand_qvel'] = sim.data.qvel[:-6].copy()*self.dt
         obs_dict['obj_pos'] = sim.data.site_xpos[self.object_sid]
         obs_dict['obj_err'] = sim.data.site_xpos[self.goal_sid] - sim.data.site_xpos[self.object_sid]
         if sim.model.na>0:
@@ -85,14 +85,16 @@ class ObjHoldFixedEnvV0(BaseV0):
 
     def get_reward_dict(self, obs_dict):
         goal_dist = np.abs(np.linalg.norm(self.obs_dict['obj_err'], axis=-1)-0.040)
+        act_mag = np.linalg.norm(self.obs_dict['act'], axis=-1)/self.sim.model.na if self.sim.model.na !=0 else 0
         gaol_th = .010
         drop = goal_dist > 0.300
 
         rwd_dict = collections.OrderedDict((
             # Optional Keys
-            ('goal_dist', goal_dist),
-            ('bonus', (goal_dist<2*gaol_th) + (goal_dist<gaol_th)),
-            ('penalty', drop),
+            ('goal_dist', -1.*goal_dist),
+            ('bonus', 1.*(goal_dist<2*gaol_th) + 1.*(goal_dist<gaol_th)),
+            ('act_reg', -1.*act_mag),
+            ('penalty', -1.*drop),
             # Must keys
             ('sparse', -goal_dist),
             ('solved', goal_dist<gaol_th),

mj_envs/envs/biomechanics/pose_v0.py
@@ -9,9 +9,10 @@ class PoseEnvV0(BaseV0):
 
     DEFAULT_OBS_KEYS = ['qpos', 'qvel', 'pose_err']
     DEFAULT_RWD_KEYS_AND_WEIGHTS = {
-        "pose": -1.0,
+        "pose": 1.0,
         "bonus": 4.0,
-        "penalty": -50,
+        "act_reg": 1.0,
+        "penalty": 50,
     }
 
     def __init__(self,
@@ -101,7 +102,7 @@ class PoseEnvV0(BaseV0):
     def get_obs_vec(self):
         self.obs_dict['t'] = np.array([self.sim.data.time])
         self.obs_dict['qpos'] = self.sim.data.qpos[:].copy()
-        self.obs_dict['qvel'] = self.sim.data.qvel[:].copy()
+        self.obs_dict['qvel'] = self.sim.data.qvel[:].copy()*self.dt
         if self.sim.model.na>0:
             self.obs_dict['act'] = self.sim.data.act[:].copy()
 
@@ -113,7 +114,7 @@ class PoseEnvV0(BaseV0):
         obs_dict = {}
         obs_dict['t'] = np.array([sim.data.time])
         obs_dict['qpos'] = sim.data.qpos[:].copy()
-        obs_dict['qvel'] = sim.data.qvel[:].copy()
+        obs_dict['qvel'] = sim.data.qvel[:].copy()*self.dt
         if sim.model.na>0:
             obs_dict['act'] = sim.data.act[:].copy()
 
@@ -122,13 +123,15 @@ class PoseEnvV0(BaseV0):
 
     def get_reward_dict(self, obs_dict):
         pose_dist = np.linalg.norm(obs_dict['pose_err'], axis=-1)
+        act_mag = np.linalg.norm(self.obs_dict['act'], axis=-1)/self.sim.model.na if self.sim.model.na !=0 else 0
         far_th = 4*np.pi/2
 
         rwd_dict = collections.OrderedDict((
             # Optional Keys
-            ('pose',    pose_dist),
-            ('bonus',   (pose_dist<self.pose_thd) + (pose_dist<1.5*self.pose_thd)),
-            ('penalty', (pose_dist>far_th)),
+            ('pose',    -1.*pose_dist),
+            ('bonus',   1.*(pose_dist<self.pose_thd) + 1.*(pose_dist<1.5*self.pose_thd)),
+            ('penalty', -1.*(pose_dist>far_th)),
+            ('act_reg', -1.*act_mag),
             # Must keys
             ('sparse',  -1.0*pose_dist),
             ('solved',  pose_dist<self.pose_thd),

mj_envs/envs/biomechanics/reach_v0.py
@@ -10,9 +10,9 @@ class ReachEnvV0(BaseV0):
 
     DEFAULT_OBS_KEYS = ['qpos', 'qvel', 'tip_pos', 'reach_err']
     DEFAULT_RWD_KEYS_AND_WEIGHTS = {
-        "reach": -1.0,
+        "reach": 1.0,
         "bonus": 4.0,
-        "penalty": -50,
+        "penalty": 50,
     }
 
     def __init__(self,
@@ -71,7 +71,7 @@ class ReachEnvV0(BaseV0):
     def get_obs_vec(self):
         self.obs_dict['t'] = np.array([self.sim.data.time])
         self.obs_dict['qpos'] = self.sim.data.qpos[:].copy()
-        self.obs_dict['qvel'] = self.sim.data.qvel[:].copy()
+        self.obs_dict['qvel'] = self.sim.data.qvel[:].copy()*self.dt
         if self.sim.model.na>0:
             self.obs_dict['act'] = self.sim.data.act[:].copy()
 
@@ -90,7 +90,7 @@ class ReachEnvV0(BaseV0):
         obs_dict = {}
         obs_dict['t'] = np.array([sim.data.time])
         obs_dict['qpos'] = sim.data.qpos[:].copy()
-        obs_dict['qvel'] = sim.data.qvel[:].copy()
+        obs_dict['qvel'] = sim.data.qvel[:].copy()*self.dt
         if sim.model.na>0:
             obs_dict['act'] = sim.data.act[:].copy()
 
@@ -105,15 +105,17 @@ class ReachEnvV0(BaseV0):
 
     def get_reward_dict(self, obs_dict):
         reach_dist = np.linalg.norm(obs_dict['reach_err'], axis=-1)
+        act_mag = np.linalg.norm(self.obs_dict['act'], axis=-1)/self.sim.model.na if self.sim.model.na !=0 else 0
         far_th = .35*len(self.tip_sids)
 
         rwd_dict = collections.OrderedDict((
             # Optional Keys
-            ('reach',   reach_dist),
-            ('bonus',   (reach_dist<.010) + (reach_dist<.005)),
-            ('penalty', (reach_dist>far_th)),
+            ('reach',   -1.*reach_dist),
+            ('bonus',   1.*(reach_dist<.010) + 1.*(reach_dist<.005)),
+            ('act_reg', -1.*act_mag),
+            ('penalty', -1.*(reach_dist>far_th)),
             # Must keys
-            ('sparse',  -1.0*reach_dist),
+            ('sparse',  -1.*reach_dist),
             ('solved',  reach_dist<.005),
             ('done',    reach_dist > far_th),
         ))