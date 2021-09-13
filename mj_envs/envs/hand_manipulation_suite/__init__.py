196f1462e2acce3a478335c7e2c357324bffd808
ShahRutav <rutavms@gmail.com>
2021-09-14 01:55:44 +0530

    Added v1 versions of tools
    a) v0 version no randomness in orientation. b) v1 version randomness in orientation as well.

--- mj_envs/envs/hand_manipulation_suite/__init__.py
+++ mj_envs/envs/hand_manipulation_suite/__init__.py
@@ -44,8 +44,12 @@ from mj_envs.envs.hand_manipulation_suite.mixtools_v0 import MixToolsEnvV0
 import os
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 env_kwargs = {
-            'model_paths' : [curr_dir + '/assets/TOOLS_ratchet.xml', curr_dir + '/assets/TOOLS_knife.xml', 
-            curr_dir + '/assets/TOOLS_screwDriver.xml', curr_dir + '/assets/TOOLS_turner.xml', curr_dir + '/assets/TOOLS_spatula.xml'],
+            'model_paths' : [curr_dir + '/assets/TOOLS_ratchet.xml', curr_dir + '/assets/TOOLS_knife.xml',
+                            curr_dir + '/assets/TOOLS_screwDriver.xml', curr_dir + '/assets/TOOLS_turner.xml',
+                            curr_dir + '/assets/TOOLS_spatula.xml'],
+            'target_pos_range' : [[0.1, 0.25], [0.0, 0.0], [0.4, 0.4]], # 0.05 0.0 0.4
+            'tool_pos_range' : [[-0.1, 0.1], [-0.2, -0.2], [0.035, 0.035]], # 0 -0.2 0.035
+            'tool_euler_range' : [[1.57, 1.57], [0, 0], [0, 0]], # TODO 3.1415927, 0, 0
             }
 register(
     id='tools-v0',
@@ -56,11 +60,14 @@ register(
 from mj_envs.envs.hand_manipulation_suite.tools_v0 import ToolsEnvV0
 
 # env_kwargs = {
-            # 'model_paths' : [curr_dir + '/assets/TOOLS_hammer.xml', curr_dir + '/assets/TOOLS_ratchet.xml', curr_dir + '/assets/TOOLS_knife.xml', 
+            # 'model_paths' : [curr_dir + '/assets/TOOLS_hammer.xml', curr_dir + '/assets/TOOLS_ratchet.xml', curr_dir + '/assets/TOOLS_knife.xml',
             # curr_dir + '/assets/TOOLS_screwDriver.xml', curr_dir + '/assets/TOOLS_turner.xml', curr_dir + '/assets/TOOLS_spatula.xml'],
             # }
 env_kwargs = {
             'model_paths' : [curr_dir + '/assets/TOOLS_hammer.xml'],
+            'target_pos_range' : [[0.1, 0.25], [0.0, 0.0], [0.4, 0.4]], # 0.05 0.0 0.4
+            'tool_pos_range' : [[-0.1, 0.1], [-0.2, -0.2], [0.035, 0.035]], # 0 -0.2 0.035
+            'tool_euler_range' : [[1.57, 1.57], [0, 0], [0, 0]], # TODO 3.1415927, 0, 0
             }
 register(
     id='tools-test-v0',
@@ -68,4 +75,30 @@ register(
     max_episode_steps=50,
     kwargs=env_kwargs,
 )
-from mj_envs.envs.hand_manipulation_suite.tools_v0 import ToolsEnvV0
+
+env_kwargs = {
+            'model_paths' : [curr_dir + '/assets/TOOLS_ratchet.xml', curr_dir + '/assets/TOOLS_knife.xml',
+                            curr_dir + '/assets/TOOLS_screwDriver.xml', curr_dir + '/assets/TOOLS_turner.xml',
+                            curr_dir + '/assets/TOOLS_spatula.xml'],
+            'target_pos_range' : [[0.1, 0.25], [0.0, 0.0], [0.4, 0.4]], # 0.05 0.0 0.4
+            'tool_pos_range' : [[-0.1, 0.1], [-0.2, -0.2], [0.035, 0.035]], # 0 -0.2 0.035
+            'tool_euler_range' : [[1.57, 1.57], [-1.57, 1.57], [0, 0]], # TODO 3.1415927, 0, 0
+            }
+register(
+    id='tools-v1',
+    entry_point='mj_envs.envs.hand_manipulation_suite:ToolsEnvV0',
+    max_episode_steps=50,
+    kwargs=env_kwargs,
+)
+env_kwargs = {
+            'model_paths' : [curr_dir + '/assets/TOOLS_hammer.xml'],
+            'target_pos_range' : [[0.1, 0.25], [0.0, 0.0], [0.4, 0.4]], # 0.05 0.0 0.4
+            'tool_pos_range' : [[-0.1, 0.1], [-0.2, -0.2], [0.035, 0.035]], # 0 -0.2 0.035
+            'tool_euler_range' : [[1.57, 1.57], [-1.57, 1.57], [0, 0]], # TODO 3.1415927, 0, 0
+            }
+register(
+    id='tools-test-v1',
+    entry_point='mj_envs.envs.hand_manipulation_suite:ToolsEnvV0',
+    max_episode_steps=50,
+    kwargs=env_kwargs,
+)
