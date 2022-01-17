d9bd88a42881efbbd3127e58641a9dccc2f3e341
Vikash Kumar <vikashplus@gmail.com>
2022-01-16 19:00:51 -0500

    FEATURE: Adding kitchen-v3 envs to env_tests

--- mj_envs/tests/test_envs.py
+++ mj_envs/tests/test_envs.py
@@ -91,5 +91,31 @@ class TestEnvs(unittest.TestCase):
         ]
         self.check_envs('FM', env_names, lite=True)
 
+    def test_kitchen(self):
+        env_names = [
+        'kitchen-v3',
+        'kitchen_close-v3',
+        'kitchen_micro_open-v3',
+        'kitchen_micro_close-v3',
+        'kitchen_rdoor_open-v3',
+        'kitchen_rdoor_close-v3',
+        'kitchen_ldoor_open-v3',
+        'kitchen_ldoor_close-v3',
+        'kitchen_sdoor_open-v3',
+        'kitchen_sdoor_close-v3',
+        'kitchen_light_on-v3',
+        'kitchen_light_off-v3',
+        'kitchen_knob4_on-v3',
+        'kitchen_knob4_off-v3',
+        'kitchen_knob3_on-v3',
+        'kitchen_knob3_off-v3',
+        'kitchen_knob2_on-v3',
+        'kitchen_knob2_off-v3',
+        'kitchen_knob1_on-v3',
+        'kitchen_knob1_off-v3'
+        ]
+        self.check_envs('Kitchen', env_names, lite=False)
+
+
 if __name__ == '__main__':
     unittest.main()
