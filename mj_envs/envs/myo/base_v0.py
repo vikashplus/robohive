fbc2fc0fc9d721a7ae2a721c6b1a99905c6ca79e
Vittorio Caggiano <caggiano@gmail.com>
2022-05-21 12:53:51 -0400

    added missing check on MVC at rest

--- mj_envs/envs/myo/base_v0.py
+++ mj_envs/envs/myo/base_v0.py
@@ -88,8 +88,12 @@ class BaseV0(env_base.MujocoEnv):
                 else:
                     self.f_load[mus_idx].append(self.sim.data.actuator_moment[mus_idx,1].copy())
 
-                f_int = np.sum(self.f_load[mus_idx]-np.max(self.f_load[mus_idx],0),0)/self.MVC_rest[mus_idx]
-                f_cem = self.MVC_rest[mus_idx]*np.exp(self.k_fatigue*f_int)
+                if self.MVC_rest[mus_idx] != 0:
+                    f_int = np.sum(self.f_load[mus_idx]-np.max(self.f_load[mus_idx],0),0)/self.MVC_rest[mus_idx]
+                    f_cem = self.MVC_rest[mus_idx]*np.exp(self.k_fatigue*f_int)
+                else:
+                    f_cem = 0
+                    
                 self.sim.model.actuator_gainprm[mus_idx,2] = f_cem
                 self.sim_obsd.model.actuator_gainprm[mus_idx,2] = f_cem
         elif self.muscle_condition == 'reafferentation':
