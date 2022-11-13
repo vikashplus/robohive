c429acede967a5ee467ca468be9df247ea108938
Vikash Kumar <vikashplus@gmail.com>
2022-11-13 12:17:49 -0500

    BugFix+Feature: use set_env_state for proper state resetting

--- mj_envs/envs/myo/myochallenge/reorient_v0.py
+++ mj_envs/envs/myo/myochallenge/reorient_v0.py
@@ -111,6 +111,7 @@ class ReorientEnvV0(BaseV0):
         ))
         rwd_dict['dense'] = np.sum([wt*rwd_dict[key] for key, wt in self.rwd_keys_wt.items()], axis=0)
 
+        # print(rwd_dict['solved'], rwd_dict['done'])
         # Sucess Indicator
         self.sim.model.site_rgba[self.success_indicator_sid, :2] = np.array([0, 2]) if rwd_dict['solved'] else np.array([2, 0])
         return rwd_dict
@@ -139,7 +140,7 @@ class ReorientEnvV0(BaseV0):
             }
         return metrics
 
-    def reset(self):
+    def reset(self, reset_qpos=None, reset_qvel=None):
         self.sim.model.body_pos[self.goal_bid] = self.goal_init_pos + \
             self.np_random.uniform( high=self.goal_pos[1], low=self.goal_pos[0], size=3)
 
@@ -162,5 +163,5 @@ class ReorientEnvV0(BaseV0):
         object_gpos = self.sim.model.geom_pos[self.object_gid0:self.object_gidn]
         self.sim.model.geom_pos[self.object_gid0:self.object_gidn] = object_gpos/abs(object_gpos+1e-16) * (abs(self.object_default_pos) + del_size)
 
-        obs = super().reset()
+        obs = super().reset(reset_qpos, reset_qvel)
         return obs
