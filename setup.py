b50b537a3bd9449b78057546fe02b08560d90858
Vincent Moens <vincentmoens@gmail.com>
2022-12-14 16:05:16 +0000

    init

--- setup.py
+++ setup.py
@@ -16,10 +16,22 @@ if sys.version_info.major != 3:
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+extra_files = package_files('mj_envs')
+
+print("package list", find_packages())
 setup(
     name='mj_envs',
     version='0.3.0',
     packages=find_packages(),
+    package_data={"": extra_files},
+    include_package_data=True,
     description='environments simulated in MuJoCo',
     long_description=read('README.md'),
     url='https://github.com/vikashplus/mj_envs.git',
@@ -27,7 +39,7 @@ setup(
     install_requires=[
         'click',
         'gym==0.13',
-        'mujoco-py<2.2,>=2.1',
+#        'mujoco-py<2.2,>=2.1',
         'termcolor',
         'sk-video',
         'flatten_dict',
