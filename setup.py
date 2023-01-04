3bc3b066816d9cfb28c1bc980b21255e49e8b0a0
Tingfan Wu <tingfan@meta.com>
2023-01-04 15:07:21 -0800

    Move alephzero / pycapnp into optional deps

--- setup.py
+++ setup.py
@@ -34,10 +34,14 @@ setup(
         'matplotlib',
         'ffmpeg',
         'absl-py',
-        'pycapnp==1.1.0',
         'r3m @ git+https://github.com/facebookresearch/r3m.git',
         # 'data_tools @ git+https://github.com/fairinternal/data_tools.git',
         'h5py==3.7.0',
-        'alephzero', # real_sense subscribers dependency
     ],
+    extras_require={
+      'a0': [
+        'pycapnp==1.1.0',
+        'alephzero', # real_sense subscribers dependency
+        ]
+    }
 )
