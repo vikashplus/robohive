7b410fcff0ea065818118f70910c3eb547acdef9
Vikash Kumar <vikashplus@gmail.com>
2023-01-24 09:23:33 -0500

    FEATURE: Adding the MyoLeg env

--- mj_envs/envs/myo/__init__.py
+++ mj_envs/envs/myo/__init__.py
@@ -284,6 +284,23 @@ register_env_with_variants(id='myoHandPoseRandom-v0',  #reconsider
     )
 
 
+# Gait Torso Reaching ==============================
+register_env_with_variants(id='myoLegReachFixed-v0',
+        entry_point='mj_envs.envs.myo.walk_v0:ReachEnvV0',
+        max_episode_steps=150,
+        kwargs={
+            'model_path': curr_dir+'/../../sims/neuromuscular_sim/gait/myolegs.xml',
+            'target_reach_range': {
+                # 'pelvis': ((-.05, -.05, .92), (0.05, 0.05, .92)),
+                'pelvis': ((-.005, -.005, .9), (0.005, 0.005, .9)),
+                # 'pelvis': ((-.005, -.005, .75), (0.005, 0.005, .9)),
+                },
+            'normalize_act': True,
+            'far_th': 0.44
+        }
+    )
+
+
 # Hand-Joint Reaching ==============================
 register_env_with_variants(id='myoHandReachFixed-v0',
         entry_point='mj_envs.envs.myo.reach_v0:ReachEnvV0',
