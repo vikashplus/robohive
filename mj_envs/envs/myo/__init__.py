a628ed942e303e73b8950b086b674cfd1829cb48
Vikash Kumar <vikashplus@gmail.com>
2023-01-28 14:08:15 -0500

    MAJOR: Upgrading to myo_sim repo

--- mj_envs/envs/myo/__init__.py
+++ mj_envs/envs/myo/__init__.py
@@ -289,7 +289,7 @@ register_env_with_variants(id='myoLegReachFixed-v0',
         entry_point='mj_envs.envs.myo.walk_v0:ReachEnvV0',
         max_episode_steps=150,
         kwargs={
-            'model_path': curr_dir+'/../../sims/neuromuscular_sim/gait/myolegs.xml',
+            'model_path': curr_dir+'/../../sims/myo_sim/gait/myolegs.xml',
             'target_reach_range': {
                 # 'pelvis': ((-.05, -.05, .92), (0.05, 0.05, .92)),
                 'pelvis': ((-.005, -.005, .9), (0.005, 0.005, .9)),
