797635c8f54be3bc0bbd49008937930001696790
Vittorio Caggiano <caggiano@fb.com>
2023-02-08 17:20:27 -0500

    fix obs vector

--- mj_envs/envs/dexman/track.py
+++ mj_envs/envs/dexman/track.py
@@ -12,6 +12,8 @@ from mj_envs.utils.xml_utils import reassign_parent
 import os
 import collections
 
+from pyquaternion import Quaternion
+
 class TrackEnv(env_base.MujocoEnv):
 
     DEFAULT_CREDIT = """\
@@ -39,7 +41,7 @@ class TrackEnv(env_base.MujocoEnv):
         processed_model_path = curr_dir+model_path[:-4]+"_processed.xml"
         with open(processed_model_path, 'w') as file:
             file.write(processed_xml)
-
+        self._object_name = object_name
         # Process obsd_model_path to import the right object
         if obsd_model_path == model_path:
             processed_obsd_model_path = processed_model_path
@@ -97,48 +99,59 @@ class TrackEnv(env_base.MujocoEnv):
             self.init_qpos[:] = self.sim.model.key_qpos[0,:]
 
 
-    def rotation_distance(q1, q2):
-        delta_quat = to_quat(q2) * to_quat(q1).inverse
+    def to_quat(self, arr):
+        if isinstance(arr, Quaternion):
+            return arr.unit
+        if len(arr.shape) == 2:
+            return Quaternion(matrix=arr).unit
+        elif len(arr.shape) == 1 and arr.shape[0] == 9:
+            return Quaternion(matrix=arr.reshape((3,3))).unit
+        return Quaternion(array=arr).unit
+
+    def rotation_distance(self, q1, q2):
+        delta_quat = self.to_quat(q2) * self.to_quat(q1).inverse
         return np.abs(delta_quat.angle)
 
-    def norm2(x):
-        return np.sum(np.square(x))
 
     def get_obs_dict(self, sim):
         obs_dict = {}
-
-        hand_ref = []
-        obj_ref = []
-        hand_pregrasp = []
+        # import ipdb; ipdb.set_trace()
+        # hand_ref = []
+        # obj_ref = []
+        # hand_pregrasp = []
 
         ## info about current hand pose + vel
-        obs_dict['curr_hand_qpos'] = sim.named.data.qpos[self._hand_name].copy()
-        obs_dict['curr_hand_qvel'] = sim.named.data.qpos[self._hand_name].copy() ## not used for now
+        obs_dict['curr_hand_qpos'] = sim.data.qpos[:-6].copy() ## assuming only 1 object and the last values are posision + rotation
+        obs_dict['curr_hand_qvel'] = sim.data.qvel[:-6].copy() ## not used for now
 
         ## info about target hand pose + vel
-        obs_dict['targ_hand_qpos'] = get_target_pose()
-        obs_dict['targ_hand_qvel'] = None ## not used for now
+        obs_dict['targ_hand_qpos'] = obs_dict['curr_hand_qpos']  #temp for testing... TBD by Vikash get_target_pose()
+        obs_dict['targ_hand_qvel'] = obs_dict['curr_hand_qvel']  #temp for testing... ## not used for now
 
         ## info about current object com + rotations
-        obs_dict['curr_obj_com'] = sim.named.data.xipos[self._object_name].copy()
-        obs_dict['curr_obj_rot'] = sim.named.data.xquat[self._object_name].copy()
+        # obs_dict['curr_obj_com'] = sim.named.data.xipos[self._object_name].copy() ## this should be with mujoco native
+        # obs_dict['curr_obj_rot'] = sim.named.data.xquat[self._object_name].copy() ## this should be with mujoco native
+        obs_dict['curr_obj_com'] = sim.data.get_body_xipos(self._object_name).copy()
+        obs_dict['curr_obj_rot'] = sim.data.get_body_xquat(self._object_name).copy()
 
         ## info about target object com + rotations
-        obj_dict['targ_obj_com']  = self.target_motion.object_pos
-        obj_dict['targ_obj_rot']  = self.target_motion.object_rot
+        obs_dict['targ_obj_com']  = obs_dict['curr_obj_com']  #temp for testing...  # TBD by Vikash self.target_motion.object_pos
+        obs_dict['targ_obj_rot']  = obs_dict['curr_obj_rot']  #temp for testing...  # TBD by Vikash self.target_motion.object_rot
 
         ## Errors
-        obj_dict['hand_qpos_err'] = np.linalg.norm(obs_dict['curr_hand_qpos']-obs_dict['targ_hand_qpos'], axis=1)
-        obj_dict['hand_qvel_err'] = np.linalg.norm(obs_dict['curr_hand_qvel']-obs_dict['targ_hand_qvel'], axis=1)
+        obs_dict['hand_qpos_err'] = np.abs(obs_dict['curr_hand_qpos']-obs_dict['targ_hand_qpos'])
+        obs_dict['hand_qvel_err'] = np.abs(obs_dict['curr_hand_qvel']-obs_dict['targ_hand_qvel'])
 
-        obj_dict['obj_com_err'] =  np.sqrt(norm2(obs_dict['curr_obj_com'] - obs_dict['targ_obj_com']))
-        obj_dict['obj_rot_err'] =  rotation_distance(obs_dict['curr_obj_rot'], obs_dict['targ_obj_rot']) / np.pi
+        obs_dict['obj_com_err'] =  np.abs(obs_dict['curr_obj_com'] - obs_dict['targ_obj_com'])
+        # import ipdb; ipdb.set_trace()
+        obs_dict['obj_rot_err'] =  self.rotation_distance(obs_dict['curr_obj_rot'], obs_dict['targ_obj_rot']) / np.pi
 
 
         obs_dict['time'] = np.array([self.sim.data.time])
         obs_dict['qp'] = sim.data.qpos.copy()
         obs_dict['qv'] = sim.data.qvel.copy()
         obs_dict['pose_err'] = obs_dict['qp'] - self.target_pose
+
         return obs_dict
 
 
