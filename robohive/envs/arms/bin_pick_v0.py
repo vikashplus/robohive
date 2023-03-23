b4489e38508d05c2e83d590245ac99ba6d0b4d4d
plancaster <plancaster@meta.com>
2023-03-22 20:03:48 -0700

    Complete bin picking env

--- robohive/envs/arms/bin_pick_v0.py
+++ robohive/envs/arms/bin_pick_v0.py
@@ -6,14 +6,16 @@ License :: Under Apache License, Version 2.0 (the "License"); you may not use th
 ================================================= """
 
 import os
+import sys
 import collections
 import gym
 import numpy as np
 
 from robohive.envs import env_base
 from robohive.physics.sim_scene import get_sim
-from robohive.utils.quat_math import euler2quat
+from robohive.utils.quat_math import euler2quat, mat2euler
 from robohive.utils.xml_utils import reassign_parent
+from robohive.utils.inverse_kinematics import qpos_from_site_pose
 
 class BinPickV0(env_base.MujocoEnv):
 
@@ -22,8 +24,8 @@ class BinPickV0(env_base.MujocoEnv):
     ]
     DEFAULT_RWD_KEYS_AND_WEIGHTS = {
         "object_dist": -1.0,
-        "target_dist": -1.0,
-        "bonus": 4.0,
+        "target_dist": -2.5,
+        "bonus": 10.0,
         "penalty": -50,
     }
 
@@ -64,13 +66,15 @@ class BinPickV0(env_base.MujocoEnv):
         # creating the sim / sim_obsd instances. Next we run through "setup"  which relies on sim / sim_obsd
         # created in __init__ to complete the setup.
         super().__init__(model_path=processed_model_path, obsd_model_path=processed_obsd_model_path, seed=seed)
+
+        self._setup(processed_model_path, **kwargs)
+
         os.remove(processed_model_path)
         if processed_obsd_model_path and processed_obsd_model_path!=processed_model_path:
             os.remove(processed_obsd_model_path)
 
-        self._setup(**kwargs)
-
     def _setup(self,
+               model_path,
                robot_site_name,
                object_site_name,
                target_site_name,
@@ -80,8 +84,8 @@ class BinPickV0(env_base.MujocoEnv):
                obs_keys=DEFAULT_OBS_KEYS,
                weighted_reward_keys=DEFAULT_RWD_KEYS_AND_WEIGHTS,
                randomize=False,
-               pos_limits = {'low': [-0.25, 0.368, 0.9, -np.pi, 0, -np.pi, 0.0],
-                             'high':[0.25,  0.72,  1.3, np.pi, 2*np.pi, 0, 0.835]},
+               pos_limits = {'eef_low': [0.368, -0.25, 0.9, -np.pi, 0, -np.pi, 0.0],
+                             'eef_high':[0.72, 0.25,  1.3, np.pi, 2*np.pi, 0, 0.835]},
                vel_limits = {'jnt': [0.15, 0.25, 0.1, 0.25, 0.1, 0.1, 0.2, 0.835],
                              'jnt_slow': [0.1, 0.25, 0.1, 0.25, 0.1, 0.1, 0.2, 0.835],
                              'eef': [0.075, 0.075, 0.15, 0.3, 0.3, 0.3, 0.835],
@@ -90,6 +94,7 @@ class BinPickV0(env_base.MujocoEnv):
                                  'high': [0.64, 0.22, 0.855]},
                min_grab_height=0.905,
                max_slow_height=1.075,
+               max_ik=3,
                **kwargs,
         ):
 
@@ -105,6 +110,20 @@ class BinPickV0(env_base.MujocoEnv):
         self.obj_pos_limits = obj_pos_limits
         self.min_grab_height = min_grab_height
         self.max_slow_height = max_slow_height
+        self.max_ik = max_ik
+        self.last_eef_cmd = None
+
+        self.ik_sim = get_sim(model_path)
+        self.pos_limits['jnt_low'] = self.sim.model.jnt_range[:self.sim.model.nu, 0]
+        self.pos_limits['jnt_high'] = self.sim.model.jnt_range[:self.sim.model.nu, 1]
+        
+        for key in pos_limits.keys():
+            pos_limits[key] = np.array(pos_limits[key])
+        for key in vel_limits.keys():
+            vel_limits[key] = np.array(vel_limits[key])
+        for key in obj_pos_limits.keys():
+            obj_pos_limits[key] = np.array(obj_pos_limits[key])
+
         super()._setup(obs_keys=obs_keys,
                        weighted_reward_keys=weighted_reward_keys,
                        reward_mode=reward_mode,
@@ -133,11 +152,11 @@ class BinPickV0(env_base.MujocoEnv):
             # Optional Keys
             ('object_dist',   object_dist),
             ('target_dist',   target_dist),
-            ('bonus',   (object_dist<.1) + (target_dist<.1) + (target_dist<.05)),
+            ('bonus',   (target_dist<.1) + (target_dist<.05)),
             ('penalty', (object_dist>far_th)),
             # Must keys
-            ('sparse',  -1.0*target_dist),
-            ('solved',  target_dist<.050),
+            ('sparse',  target_dist<.075),
+            ('solved',  target_dist<.075),
             ('done',    object_dist > far_th),
         ))
         rwd_dict['dense'] = np.sum([wt*rwd_dict[key] for key, wt in self.rwd_keys_wt.items()], axis=0)
@@ -151,7 +170,6 @@ class BinPickV0(env_base.MujocoEnv):
         if self.randomize:
             # target location
             self.sim.model.site_pos[self.target_sid] = self.np_random.uniform(high=self.target_xyz_range['high'], low=self.target_xyz_range['low'])
-            self.sim_obsd.model.site_pos[self.target_sid] = self.sim.model.site_pos[self.target_sid]
 
             # Randomize obj pose
             obj_jid = self.sim.model.joint_name2id(self.object_site_name)
@@ -162,11 +180,118 @@ class BinPickV0(env_base.MujocoEnv):
             self.sim.forward()
 
         obs = super().reset(reset_qpos, reset_qvel, **kwargs)
+
+        cur_pos = self.sim.data.site_xpos[self.grasp_sid]
+        cur_rot = mat2euler(self.sim.data.site_xmat[self.grasp_sid].reshape(3,3).transpose())
+        cur_rot[np.abs(cur_rot-np.array([np.pi,0.0,0.0]))>np.abs(cur_rot+2*np.pi-np.array([np.pi,0.0,0.0]))] += 2*np.pi
+        cur_rot[np.abs(cur_rot-np.array([np.pi,0.0,0.0]))>np.abs(cur_rot-2*np.pi-np.array([np.pi,0.0,0.0]))] -= 2*np.pi
+        self.last_eef_cmd = np.concatenate([cur_pos,
+                                            cur_rot,
+                                            [self.sim.data.qpos[7]]])
+
         return obs
 
-    # def viewer_setup(self):
-    #     self.sim.renderer.set_free_camera_settings(
-    #             distance=1.25,
-    #             azimuth=-90,
-    #             elevation=-20,
-    #     )
+    def get_ik_action(self, eef_pos, eef_quat):
+        for i in range(self.max_ik):
+
+            self.ik_sim.data.qpos[:7] = np.random.normal(self.sim.data.qpos[:7], i*0.1)
+
+            self.ik_sim.data.qpos[2] = 0.0
+            self.ik_sim.data.qpos[3] = -2.0
+            self.ik_sim.forward()
+
+            ik_result = qpos_from_site_pose(physics = self.ik_sim,
+                                            site_name = self.sim.model.site_id2name(self.grasp_sid),
+                                            target_pos= eef_pos,
+                                            target_quat= eef_quat,
+                                            inplace=False,
+                                            regularization_strength=1.0,
+                                            is_hardware=self.robot.is_hardware)
+
+            if ik_result.success:
+                break
+        return ik_result
+    
+    def step(self, a, **kwargs):
+        assert(len(a.shape) == 1)
+        assert(a.shape[0] == self.sim.model.nu or a.shape[0] == 7)
+        
+        if a.shape[0] == self.sim.model.nu:
+            action = a
+            
+            # Enforce joint velocity limits
+            if self.normalize_act:
+                action = (0.5*action+0.5)*(self.pos_limits['jnt_high']-self.pos_limits['jnt_low'])+self.pos_limits['jnt_low']
+            action = np.clip(action, 
+                             self.sim.data.qpos[:self.sim.model.nu]-self.vel_limits['jnt'], 
+                             self.sim.data.qpos[:self.sim.model.nu]+self.vel_limits['jnt'])
+            if self.normalize_act:
+                action = 2*(((action - self.pos_limits['jnt_low'])/(self.pos_limits['jnt_high']-self.pos_limits['jnt_low']))-0.5)            
+        else:  
+            # Un-normalize cmd
+            eef_cmd = (0.5*a+0.5)*(self.pos_limits['eef_high']-self.pos_limits['eef_low'])+self.pos_limits['eef_low']
+            eef_cmd = np.clip(eef_cmd, low=self.pos_limits['eef_low'], high=self.pos_limits['eef_high'])
+
+            # Get current position and rotation of eef
+            cur_rot = mat2euler(self.sim.data.site_xmat[self.grasp_sid].reshape(3,3).transpose())
+            cur_rot[np.abs(cur_rot-eef_cmd[3:6])>np.abs(cur_rot+2*np.pi-eef_cmd[3:6])] += 2*np.pi
+            cur_rot[np.abs(cur_rot-eef_cmd[3:6])>np.abs(cur_rot-2*np.pi-eef_cmd[3:6])] -= 2*np.pi
+            cur_pose = np.concatenate([self.sim.data.site_xpos[self.grasp_sid],
+                                       cur_rot,
+                                       [self.sim.data.qpos[7]]])
+                
+            # Enforce cartesian velocity limits
+            if cur_pose[2] < self.max_slow_height:# and self.robot.is_hardware:
+                eef_cmd = np.clip(eef_cmd, cur_pose-self.vel_limits['eef_slow'], cur_pose+self.vel_limits['eef_slow'])
+            else:
+                eef_cmd = np.clip(eef_cmd, cur_pose-self.vel_limits['eef'], cur_pose+self.vel_limits['eef'])
+
+            # Exponential moving average to limit jerk
+            assert self.last_eef_cmd is not None
+            eef_cmd[:6] = 0.25*eef_cmd[:6] + 0.75*self.last_eef_cmd[:6]
+
+            # Prepare for IK, execute last successful command if IK fails
+            eef_pos = eef_cmd[:3]
+            eef_elr = eef_cmd[3:6]
+            eef_quat= euler2quat(eef_elr)
+
+            ik_result = self.get_ik_action(eef_pos, eef_quat)
+            ik_success = ik_result.success
+            if not ik_success:
+                eef_cmd = self.last_eef_cmd
+                eef_pos = eef_cmd[:3]
+                eef_elr = eef_cmd[3:6]
+                eef_quat = euler2quat(eef_elr)
+                ik_result = self.get_ik_action(eef_pos, eef_quat)
+            
+            action = ik_result.qpos[:self.sim.model.nu]
+            
+            # Check that we are not initiating a grasp at too low of height
+            if ((eef_cmd[6] > sys.float_info.epsilon) and 
+                self.sim.data.site_xpos[self.grasp_sid][2] < self.min_grab_height):
+                print('Cant grasp this low, z = {}'.format(self.sim.data.site_xpos[self.grasp_sid][2]))
+                action[7:self.sim.model.nu] = 0.0
+            else:
+                action[7:self.sim.model.nu] = eef_cmd[6]
+
+            self.last_eef_cmd = eef_cmd
+
+            # Enforce joint velocity limits
+            #if self.robot.is_hardware:
+            if cur_pose[2] < self.max_slow_height:
+                action = np.clip(action, 
+                                self.sim.data.qpos[:self.sim.model.nu]-self.vel_limits['jnt_slow'], 
+                                self.sim.data.qpos[:self.sim.model.nu]+self.vel_limits['jnt_slow'])
+            else:
+                action = np.clip(action, 
+                                 self.sim.data.qpos[:self.sim.model.nu]-self.vel_limits['jnt'], 
+                                 self.sim.data.qpos[:self.sim.model.nu]+self.vel_limits['jnt'])
+
+            if self.normalize_act:
+                action = 2*(((action - self.pos_limits['jnt_low'])/(self.pos_limits['jnt_high']-self.pos_limits['jnt_low']))-0.5)                     
+        
+        return super().step(action, **kwargs)
+        
+class BinPickPolicy():
+    def __init__(self, env, seed):
+        self.env = env
