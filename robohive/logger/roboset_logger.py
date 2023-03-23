68ec799a24ef494d17117660d740a83b34e7a5b5
Vikash Kumar <vikashplus@gmail.com>
2023-03-23 19:35:18 -0400

    FEATURE: Explicitely keeping track of  logger type

--- robohive/logger/roboset_logger.py
+++ robohive/logger/roboset_logger.py
@@ -1,8 +1,12 @@
-from robohive.logger.grouped_datasets import Trace
+from robohive.logger.grouped_datasets import Trace, TraceType
 import numpy as np
 import json
 
 class RoboSet_Trace(Trace):
+
+    def __init__(self, trace_type=TraceType.ROBOSET, **kwargs):
+        super().__init__(trace_type=trace_type, **kwargs)
+
 
     # parse path from robohive format into robopen dataset format
     def path2dataset(self, path:dict, config_path=None)->dict:
