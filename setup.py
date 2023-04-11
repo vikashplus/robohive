f1e2f14f339763e9661de961dc6a20047f1ddd07
vmoens <vincentmoens@gmail.com>
2023-04-11 14:57:46 +0100

    init

--- setup.py
+++ setup.py
@@ -23,37 +23,55 @@ def package_files(directory):
             paths.append(os.path.join('..', path, filename))
     return paths
 
-extra_files = package_files('mj_envs')
+def parse_args(argv: List[str]) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="mj_envs setup")
+    parser.add_argument(
+        "--package_name",
+        type=str,
+        default="mj_envs",
+        help="the name of this output wheel",
+    )
+    return parser.parse_known_args(argv)
 
-setup(
-    name='mj_envs',
-    version='0.4.0',
-    packages=find_packages(),
-    package_data={"": extra_files},
-    include_package_data=True,
-    description='environments simulated in MuJoCo',
-    long_description=read('README.md'),
-    long_description_content_type="text/markdown",
-    url='https://github.com/vikashplus/mj_envs.git',
-    author='Movement Control Lab, UW',
-    install_requires=[
-        'click',
-        'gym==0.13',
-#        'mujoco-py<2.2,>=2.1',
-        'termcolor',
-        'sk-video',
-        'flatten_dict',
-        'matplotlib',
-        'ffmpeg',
-        'absl-py',
-        'r3m @ git+https://github.com/facebookresearch/r3m.git',
-        # 'data_tools @ git+https://github.com/fairinternal/data_tools.git',
-        'h5py==3.7.0',
-    ],
-    extras_require={
-      'a0': [
-        'pycapnp==1.1.0',
-        'alephzero', # real_sense subscribers dependency
-        ]
-    }
-)
+def _main(argv):
+
+    args, unknown = parse_args(argv)
+    name = args.package_name
+
+    extra_files = package_files('mj_envs')
+
+    setup(
+        name=name,
+        version='0.4.0',
+        packages=find_packages(),
+        package_data={"": extra_files},
+        include_package_data=True,
+        description='environments simulated in MuJoCo',
+        long_description=read('README.md'),
+        long_description_content_type="text/markdown",
+        url='https://github.com/vikashplus/mj_envs.git',
+        author='Movement Control Lab, UW',
+        install_requires=[
+            'click',
+            'gym==0.13',
+    #        'mujoco-py<2.2,>=2.1',
+            'termcolor',
+            'sk-video',
+            'flatten_dict',
+            'matplotlib',
+            'ffmpeg',
+            'absl-py',
+            'r3m @ git+https://github.com/facebookresearch/r3m.git',
+            # 'data_tools @ git+https://github.com/fairinternal/data_tools.git',
+            'h5py==3.7.0',
+        ],
+        extras_require={
+          'a0': [
+            'pycapnp==1.1.0',
+            'alephzero', # real_sense subscribers dependency
+            ]
+        }
+    )
+
+if __name__ == "__main__":
+    _main(sys.argv[1:])
