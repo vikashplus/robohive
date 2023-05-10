ad8d96531ee2e519be29ca744007bc947b973e05
Vikash Kumar <vikashplus@gmail.com>
2023-05-10 16:15:20 -0400

    HMS+BUGFIX: reset qpos qvel when specified had a bug during comparision with None

--- robohive/envs/hands/relocate_v1.py
+++ robohive/envs/hands/relocate_v1.py
@@ -138,8 +138,8 @@ class RelocateEnvV1(env_base.MujocoEnv):
 
     def reset(self, reset_qpos=None, reset_qvel=None, **kwargs):
         self.sim.reset()
-        qp = self.init_qpos.copy() if reset_qpos==None else reset_qpos
-        qv = self.init_qvel.copy() if reset_qvel==None else reset_qvel
+        qp = self.init_qpos.copy() if reset_qpos is None else reset_qpos
+        qv = self.init_qvel.copy() if reset_qvel is None else reset_qvel
         self.robot.reset(reset_pos=qp, reset_vel=qv, **kwargs)
 
 
