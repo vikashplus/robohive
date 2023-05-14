548fbddeb0ec1bee40e2bcacf5305a79c922cd7a
Vikash Kumar <vikashplus@gmail.com>
2023-05-14 18:47:24 -0400

    MYODEX+FEATURE: Adding the myodex envs. Envs and the replays are in working conditions. The motion files are the old files used in the repo. These files needs to be reparsed into the new env convensions. Adding new envs into the test as well

--- robohive/tests/test_myo.py
+++ robohive/tests/test_myo.py
@@ -4,6 +4,8 @@ Authors  :: Vikash Kumar (vikashplus@gmail.com), Vittorio Caggiano (caggiano@gma
 ================================================= """
 
 import unittest
+import click
+import click.testing
 from robohive.tests.test_envs import TestEnvs
 from robohive import robohive_myo_suite
 
@@ -11,5 +13,24 @@ class TestMyo(TestEnvs):
     def test_envs(self):
         self.check_envs('Myo Suite', robohive_myo_suite)
 
+    def test_myodex(self):
+        env_names="MyoHandAirplaneFly-v0,MyoHandAirplaneLift-v0,MyoHandAirplanePass-v0,MyoHandAlarmclockLift-v0,MyoHandAlarmclockPass-v0,MyoHandAlarmclockSee-v0,MyoHandAppleLift-v0,MyoHandApplePass-v0,MyoHandBananaPass-v0,MyoHandBinocularsPass-v0,MyoHandCupDrink-v0,MyoHandCupPass-v0,MyoHandCupPour-v0,MyoHandDuckInspect-v0,MyoHandDuckLift-v0,MyoHandDuckPass-v0,MyoHandElephantLift-v0,MyoHandElephantPass-v0,MyoHandEyeglassesPass-v0,MyoHandFlashlightLift-v0,MyoHandFlashlight1On-v0,MyoHandFlashlight2On-v0,MyoHandFlashlightPass-v0,MyoHandFlutePass-v0,MyoHandFryingpanCook-v0,MyoHandHammerPass-v0,MyoHandHammerUse-v0,MyoHandHandInspect-v0,MyoHandHandPass-v0,MyoHandHeadphonesPass-v0,MyoHandKnifeChop-v0,MyoHandKnifeLift-v0,MyoHandLightbulbPass-v0,MyoHandMouseLift-v0,MyoHandMouseUse-v0,MyoHandMugDrink3-v0,MyoHandPiggybankUse-v0,MyoHandScissorsUse-v0,MyoHandSpheremediumLift-v0,MyoHandStampStamp-v0,MyoHandStanfordbunnyInspect-v0,MyoHandStaplerLift-v0,MyoHandToothbrushLift-v0,MyoHandToothpasteLift-v0,MyoHandToruslargeInspect-v0,MyoHandTrainPlay-v0,MyoHandWatchLift-v0,MyoHandWaterbottleLift-v0,MyoHandWaterbottleShake-v0,MyoHandWineglassDrink1-v0,MyoHandWineglassDrink2-v0,MyoHandWineglassLift-v0,MyoHandWineglassPass-v0".split(",")
+
+        # Check the envs
+        self.check_envs('MyoDex', env_names)
+
+        # Check trajectory playback
+        from robohive.envs.tcdm.playback_traj import playback_default_traj
+        for env in env_names:
+            print(f"Testing reference motion playback on: {env}")
+            runner = click.testing.CliRunner()
+            result = runner.invoke(playback_default_traj, ["--env_name", env, \
+                                                        "--horizon", -1, \
+                                                        "--num_playback", 1, \
+                                                        "--render", "none"])
+
+
 if __name__ == '__main__':
     unittest.main()
+
+
