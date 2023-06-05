3dfe8046120f541b2ce8466e1c530a910715ad1a
Vikash Kumar <vikashplus@gmail.com>
2023-06-05 12:27:13 -0400

    ARMS:FEATURE: Preparing visual envs for arms - Adding proprio keys - Separating robot and obs keys - Adding cameras to support standard exteroception

--- robohive/tests/test_arms.py
+++ robohive/tests/test_arms.py
@@ -8,24 +8,29 @@ class TestArms(TestEnvs):
             'FrankaReachRandom-v0',
             'FrankaPushFixed-v0',
             'FrankaPushRandom-v0',
-            'FetchReachFixed-v0',
-            'FetchReachRandom-v0',
             'FrankaPickPlaceFixed-v0',
             'FrankaPickPlaceRandom-v0'
             ]
-        self.check_envs('Arms', env_names)
+        self.check_envs('Arms(Franka)', env_names)
+
+        env_names = [
+            'FetchReachFixed-v0',
+            'FetchReachRandom-v0',
+            ]
+        self.check_envs('Arms(Fetch)', env_names)
 
     def test_arms_visual(self):
         env_names = [
             'FrankaReachRandom_v2d-v0',
-            'FrankaReachRandom_vr3m18-v0',
-            'FrankaReachRandom_vr3m34-v0',
-            'FrankaReachRandom_vr3m50-v0',
-            'FrankaReachRandom_vrrl18-v0',
-            'FrankaReachRandom_vrrl34-v0',
-            'FrankaReachRandom_vrrl50-v0',
+            'FrankaPushRandom_v2d-v0',
+            'FrankaPickPlaceRandom_v2d-v0'
             ]
-        self.check_envs('Arms(visual)', env_names)
+        self.check_envs('Arms(Franka)(Visual)', env_names)
+
+        env_names = [
+            'FetchReachRandom_v2d-v0',
+            ]
+        self.check_envs('Arms(Fetch)(Visual)', env_names)
 
 if __name__ == '__main__':
     unittest.main()
