a4955cfef1b9170cacb693dcba28fe788870a7f1
Vikash Kumar <vikashplus@gmail.com>
2023-07-11 19:17:46 -0400

    KETCHEN CLEANUP: Upgrading substep2 and substep9 envs to be compatible with FIXED and RANDOM convensions. Removing the franka_kitchen_v1.py

--- robohive/envs/multi_task/substeps2/__init__.py
+++ robohive/envs/multi_task/substeps2/__init__.py
@@ -5,38 +5,21 @@ Source  :: https://github.com/vikashplus/robohive
 License :: Under Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 ================================================= """
 
-import os
-from gym.envs.registration import register
-
-CURR_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# Kitchen-V3 ============================================================================
-# In this version of the environment, the observations consist of the
-# distance between end effector and all relevent objects in the scene
-
 print("RoboHive:> Registering Multi-Task (2 subtasks) Envs")
-from robohive.envs.multi_task.common.franka_kitchen_v1 import KitchenFrankaFixed, KitchenFrankaRandom, KitchenFrankaDemo
+from robohive.envs.multi_task.substeps1.franka_kitchen import register_all_env_variants
 
-MODEL_PATH = CURR_DIR + "/../common/kitchen/franka_kitchen.xml"
-CONFIG_PATH = CURR_DIR + "/../common/kitchen/franka_kitchen.config"
-
-DEMO_ENTRY_POINT = "robohive.envs.multi_task.common.franka_kitchen_v1:KitchenFrankaDemo"
-RANDOM_ENTRY_POINT = "robohive.envs.multi_task.common.franka_kitchen_v1:KitchenFrankaRandom"
-FIXED_ENTRY_POINT = "robohive.envs.multi_task.common.franka_kitchen_v1:KitchenFrankaFixed"
-ENTRY_POINT = RANDOM_ENTRY_POINT
-
-obs_keys_wt = {"robot_jnt": 1.0, "objs_jnt": 1.0, "obj_goal": 1.0, "end_effector": 1.0}
-for site in KitchenFrankaFixed.OBJ_INTERACTION_SITES:
-    obs_keys_wt[site + "_err"] = 1.0
+# ===================================================================
+# ENVs are provided in three VARIANTs (controlling env's behavior)
+# - Fixed-v4:       State based stationary environments with no randomization
+# - Random-v4:      State based environments with random robot initialization (joint pose + relative position wrt to kitchen)
+# - Random_v2d-v4:  Visual environment with random robot initialization (joint pose + relative position wrt to kitchen)
+# ===================================================================
 
 # Kitchen (close microwave and slidedoor)
-register(
-    id="kitchen_micro_slide_close-v3",
-    entry_point=ENTRY_POINT,
+register_all_env_variants(
+    task_id="FK1_MicroSlideClose",
     max_episode_steps=50,
-    kwargs={
-        "model_path": MODEL_PATH,
-        "config_path": CONFIG_PATH,
+    task_configs={
         "obj_goal": {
             "micro0joint": 0,
             "slidedoor_joint": 0,
@@ -45,6 +28,5 @@ register(
             "micro0joint": -1.25,
             "slidedoor_joint": 0.44,
         },
-        "obs_keys_wt": obs_keys_wt,
     },
 )
