104c4e30a55e52a54dd9038c236b1c44b14d41f4
Vikash Kumar <vikashplus@gmail.com>
2023-07-15 09:07:11 -0400

    Removing TCDM tests as its not ready

--- robohive/tests/test_all.py
+++ robohive/tests/test_all.py
@@ -5,7 +5,7 @@ from robohive.tests.test_hands import TestHMS
 from robohive.tests.test_multitask import TestKitchen
 from robohive.tests.test_myo import TestMyo
 from robohive.tests.test_claws import TestClaws
-from robohive.tests.test_tcdm import TestTCDM
+# from robohive.tests.test_tcdm import TestTCDM
 
 from robohive.tests.test_examine_env import TestExamineEnv
 from robohive.tests.test_examine_robot import TestExamineRobot
