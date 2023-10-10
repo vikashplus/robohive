ef6f2c3deb93555d779bb3f9af0b3c21414c6bc0
Vikash Kumar <vikashplus@gmail.com>
2023-10-10 11:55:09 -0400

    BUGFIX: Updating the simHive hash

--- robohive_init.py
+++ robohive_init.py
@@ -124,7 +124,7 @@ def fetch_simhive():
                 clone_path=simhive_path)
 
     fetch_git(repo_url="https://github.com/MyoHub/myo_sim.git",
-                commit_hash="fd4e15ad6797db9351a921a8c8394a7070ba5541",
+                commit_hash="aff0bc096d98085ee0a6befd613cc9fbff024944",
                 clone_directory="myo_sim",
                 clone_path=simhive_path)
 
